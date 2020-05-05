1401d001dbba8c49f6d3127cb48a67599ac4369f
Ashish S <ashishjsharda@gmail.com>
2020-05-05 08:53:10 -0400

    Create Lambda19.java

---
+++ Lambda19.java
@@ -0,0 +1,26 @@
+/**
+ * Number Interface
+ */
+interface Number{
+    boolean test(int num);
+}
+
+/**
+ * Example using Lambda expression
+ * @author ashish
+ */
+public class Lambda19 {
+    public static void main(String[] args) {
+
+        Number even=(x) ->{if (x%2==0){
+        return true;}
+        return false;
+        };
+        System.out.println("Number is an even number"+even.test(10));
+        Number odd=(x) ->{if (x%2!=0){
+            return true;}
+            return false;
+        };
+        System.out.println("Number is an odd number"+ odd.test(11));
+    }
+}
