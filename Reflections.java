18dd0cf9726d67cbc695e68e118062e2fedc5e5f
Ashish S <ashishjsharda@gmail.com>
2020-12-14 07:31:46 -0500

    Create Reflections.java

---
+++ Reflections.java
@@ -0,0 +1,10 @@
+/**
+ * Reflections Example 1
+ * @author ashish
+ */
+public class Reflections {
+    public static void main(String[] args) {
+        Class<?>c1=int.class;
+        System.out.println(c1.getName());
+    }
+}
